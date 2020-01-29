2755a19f000f5e54a26cd5971055ae3d00c7455c
Jimmer942 <942@holbertonschool.com>
2020-01-29 15:35:18 -0500

    cities.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,7 @@
 from flask import Blueprint, jsonify
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
 
 app_views = Blueprint('/api/v1', __name__)
 
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,11 +4,12 @@
 from flask import Blueprint, jsonify, abort, request
 from models import storage
 from models.city import City
-from api.v1.views import app_views
+
 import json
 
 
 def init_cities():
+    from api.v1.views import app_views
 
     @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
     def all_cities_in_state(state_id=None):
