c26e8546033944e35b27c0b500cd4b0ed590d480
Jimmer942 <942@holbertonschool.com>
2020-01-29 15:20:30 -0500

    abort(404)

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,12 +7,10 @@ from models.state import State
 from api.v1.views import app_views
 import json
 
-re = '/states/<state_id>/cities'
-
 
 def init_cities():
 
-    @app_views.route(re, methods=['GET'], strict_slashes=False)
+    @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
     def all_cities_in_state(state_id=None):
         if id is not None and storage.get("State", id) is not None:
             cities = []
@@ -20,3 +18,5 @@ def init_cities():
                 if city.state_id == str(state_id):
                     cities.append(city.to_dict())
             return jsonify(cities)
+        else:
+            abort(404)
