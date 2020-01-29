f6d7c9aaad66bee0f56711768dc3fd547a507ae3
gefranco <933@holbertonschool.com>
2020-01-29 16:10:11 +0000

    [add] host and port variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
-
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
