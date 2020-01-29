a67905c161617a165a63d8621375b555000704aa
Jimmer942 <942@holbertonschool.com>
2020-01-29 15:26:12 -0500

    cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,7 +3,7 @@
 
 from flask import Blueprint, jsonify, abort, request
 from models import storage
-from models.state import State
+from models.city import City
 from api.v1.views import app_views
 import json
 
