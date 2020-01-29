2ade433fefce85b6c339f3e5c0c674f6fcc97f0d
Jimmer942 <942@holbertonschool.com>
2020-01-29 11:43:36 -0500

    move variables and import Blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
-'''
+""" App """
 from flask import Flask, make_response, jsonify
+from flask import Blueprint
 from models import storage
 from api.v1.views import app_views
 import os
@@ -9,21 +9,21 @@ import os
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
 def teardown_db(self):
-    ''''''
+    """ close storage """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    ''''''
+    """ Error 404 """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+    port = os.getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
