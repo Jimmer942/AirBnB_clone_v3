3e9e8595abc05d0a26976089ad7ab37ebeeeaa03
Jimmer942 <942@holbertonschool.com>
2020-01-30 14:36:55 -0500

    amenities.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,6 +7,7 @@ from flask import Blueprint, jsonify
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 app_views = Blueprint('/api/v1', __name__)
