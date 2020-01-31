2a02e90c9a6fdb723bcad3bbc91f0409e5e25cb5
Jimmer942 <942@holbertonschool.com>
2020-01-30 21:57:48 -0500

    users.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,6 +8,7 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 app_views = Blueprint('/api/v1', __name__)
@@ -16,5 +17,6 @@ index.init()
 states.init_states()
 cities.init_cities()
 amenities.init_amenities()
+users.init_users()
 places.init_places()
 places_reviews.init_places_reviews()
